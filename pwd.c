809c4bd6233602dc1909c5554480a3dd79aad7de
aabhinavg <78288544+aabhinavg@users.noreply.github.com>
2021-02-20 21:38:03 +0530

    Update pwd.c

--- pwd.c
+++ pwd.c
@@ -1,20 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h>    /* header file containing defination of getcwd*/
 
 int main(void) {
     long current_location;
     char *buf;
 
-    current_location = pathconf(".", _PC_PATH_MAX);
-    buf = malloc(current_location * sizeof(*buf));
-    if (getcwd(buf, current_location) == NULL) 
+    current_location = pathconf(".", _PC_PATH_MAX);/*The maximum length of a relative pathname when path or fd is the
+              current   working   directory (open terminal and type man 3 pathconf to know both pathconf and _PC_PATH_MAX*/
+
+    buf = malloc(current_location * sizeof(*buf));/*allocation the buffer size*/
+    if (getcwd(buf, current_location) == NULL)  /*getcwd is an system call which copy absolute path of current working directory (open terminal and type man 3 getcwd to know it in detail)*/
     {
                 perror("getcwd");
                  exit(EXIT_FAILURE);
     } 
     else 
-    {           printf("%s\n", buf);    }
+    {           printf("%s\n", buf);    }/*print current working directory*/
     
     free(buf);
     return EXIT_SUCCESS;
