a6beba323fc7ad210684d3961f001349f9337881
aabhinavg <78288544+aabhinavg@users.noreply.github.com>
2021-02-20 21:12:07 +0530

    Create pwd.c

---
+++ pwd.c
@@ -0,0 +1,23 @@
+#define _XOPEN_SOURCE 700
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+    long n;
+    char *buf;
+
+    n = pathconf(".", _PC_PATH_MAX);
+    assert(n != -1);
+    buf = malloc(n * sizeof(*buf));
+    assert(buf);
+    if (getcwd(buf, n) == NULL) {
+        perror("getcwd");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("%s\n", buf);
+    }
+    free(buf);
+    return EXIT_SUCCESS;
+}
